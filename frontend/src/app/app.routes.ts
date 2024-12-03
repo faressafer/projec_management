4fe7dd9bfb443927ec539cb5b490c0cdfc5c3cce
FaresOps <faressafer05@gmail.com>
2024-12-03 22:08:38 +0100

    assetss upadet

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,9 +6,9 @@ import { NotfoundComponent } from './pages/notfound/notfound.component';
 export const routes: Routes = [
 
 
-    { path: '', redirectTo: 'dashboard' , pathMatch: 'full'},
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'login', component: LoginComponent},
-    {path: '**', component: NotfoundComponent},
+    // { path: '', redirectTo: 'dashboard' , pathMatch: 'full'},
+    // {path: 'dashboard', component: DashboardComponent},
+    // {path: 'login', component: LoginComponent},
+    // {path: '**', component: NotfoundComponent},
 
 ];
